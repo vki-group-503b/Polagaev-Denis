31cb0b6840053cb0651d5bce63692814f5b9cdd3
DenisPolagaev <32395325+DenisPolagaev@users.noreply.github.com>
2017-10-17 14:48:24 +0700

    Update Main.java

--- src/ru/hci/nsu/polagaev/javalabs/lab2/Main.java
+++ src/ru/hci/nsu/polagaev/javalabs/lab2/Main.java
@@ -1,35 +1,14 @@
 package ru.hci.nsu.polagaev.javalabs.lab2;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Array arr =new Array();
-        System.out.print("Введите кол-во чисел в массиве: ");
-        int[] array;
-        if (scan.hasNextInt()) {
-            array = new int[scan.nextInt()];
-            for (int i = 0; i < array.length; i++) {
-                System.out.println("Введите число: " + (i + 1));
-                if (scan.hasNextInt()) {
-                    array[i] = scan.nextInt();
-                } else {
-                    System.out.println("Вы ввели не число!");
-                    return;
-                }
-            }
-            Arrays.sort(array);
-        } else {
-            System.out.println("Вы ввели не число!");
-            return;
-        }
-        arr.min(array);
-        arr.max(array);
-        arr.avg(array);
-        arr.median(array);
-        arr.geometric(array);
+    public static void main(String[] args) throws Exception {
+        System.out.println("Введите кол-во чисел: ");
+        Array arr = new Array();
+        arr.min();
+        arr.max();
+        arr.avg();
+        arr.geometric();
+        arr.median();
     }
 }
