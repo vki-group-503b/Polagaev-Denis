03e5060617f3e89eeb0783b250bf4d1e6a445061
DenisPolagaev <32395325+DenisPolagaev@users.noreply.github.com>
2017-10-17 20:58:30 +0700

    Add files via upload

---
+++ src/ru/hci/nsu/polagaev/javalabs/lab5/Formatter.java
@@ -0,0 +1,10 @@
+﻿package ru.hci.nsu.polagaev.javalabs.lab5;
+
+class Formatter {
+    public static void main(String[] args) {
+        double t = 17.36;
+        String p = new String();
+        String s = p.format("Температура в новосибирске = ${0}.",t);
+        System.out.println(s);
+    }
+}
